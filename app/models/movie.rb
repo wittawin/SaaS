38083716f909600a151f4ae04cfdcf5ba8d9bf79
Wittawin Kahuttanaseth <wittawin.k@gmail.com>
2015-11-20 00:48:13 +0700

    Finish Validation

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,4 +1,5 @@
 class Movie < ActiveRecord::Base
+  attr_accessible :title, :rating, :description, :release_date
   def self.all_ratings ; %w[G PG PG-13 R NC-17] ; end #  shortcut: array of strings
   validates :title, :presence => true
   validates :release_date, :presence => true
